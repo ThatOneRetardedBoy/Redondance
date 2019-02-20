f092cc9d8e7cbcc9025c170d0bf1301e15ce9ca3
Maxime B <maxime.boulmeck@gmail.com>
2019-02-20 01:46:18 +0100

    First commit

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+//#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace std;
+using namespace cv;
+int main() {
+
+    Mat img1 = imread("image1.png");
+    Mat img2 = imread("image2.png");
+
+    // calc the difference
+    Mat diff;
+    absdiff(img1, img2, diff);
+
+    // Get the mask if difference greater than th
+    int th = 100;  // 0
+    Mat mask(img1.size(), CV_8UC1);
+    for(int j=0; j<diff.rows; ++j) {
+        for(int i=0; i<diff.cols; ++i){
+            cv::Vec3b pix = diff.at<cv::Vec3b>(j,i);
+            int val = (pix[0] + pix[1] + pix[2]);
+            if(val>th){
+                mask.at<unsigned char>(j,i) = 255;
+            }
+        }
+    }
+
+    // get the foreground
+    Mat image;
+    bitwise_and(img1, img2, image, mask);
+
+    // display
+    //resize(image, image, Size(image.cols/4, image.rows/4)); // to half size or even smaller
+    //namedWindow( "Display frame", WINDOW_AUTOSIZE);
+    //imshow("res", image);
+    imwrite("resultat_diff.png", image);
+    waitKey();
+    return 0;
+}
+
